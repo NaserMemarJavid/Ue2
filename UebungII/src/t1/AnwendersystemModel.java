f596706d283e7c28d96cc7cbe5b6665fdd166837
nasse <nasse@Merida>
2022-10-29 20:12:41 +0200

    your turn!

---
+++ UebungII/src/t1/AnwendersystemModel.java
@@ -0,0 +1,15 @@
+package t1;
+
+import java.io.*;
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class AnwendersystemModel {
+
+}
---
+++ UebungII/src/t1/Main.java
@@ -0,0 +1,20 @@
+package t1;
+
+import java.io.*;
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
